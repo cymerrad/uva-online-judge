a809aa75369729c5bd5c9621a783ea290760de2a
Radosław Cymer <cymerrad@gmail.com>
2019-10-12 22:36:57 +0200

    Ugly Numbers

--- 136/lol.hs
+++ 136/lol.hs
@@ -1,19 +1,43 @@
-module Main where
+{- stack
+  runghc
+  --package data-ordlist
+-}
 
+module Main where
+import           Prelude
+import           System.IO
 import           Data.List.Ordered
 import           Data.Ord
 -- data-ordlist
-norm (x, y, z) =
-  sqrt (fromIntegral x ^ 2 + fromIntegral y ^ 2 + fromIntegral z ^ 2)
+
+main :: IO ()
+
+main = do
+  -- handle   <- openFile "nums.txt" WriteMod
+  -- contents <- hGetContents handle
+  -- hClose handle
+  -- writeFile "num.txt"
+  --   $ foldl (\a b -> a ++ b ++ "\n") "" (map show (take 10000 uglyNums))
+  print (uglyNums !! 1498)
+  print (uglyNums !! 1499)
+  print (uglyNums !! 1500)
+  print (uglyNums !! 1501)
+  return ()
+
+
+ugly :: (Integer, Integer, Integer) -> Integer
+ugly (x, y, z) = 2 ^ x * 3 ^ y * 5 ^ z
+norm = ugly -- UGLY IS THE NORM!
+-- norm (x,y,z) = sqrt (fromIntegral x ^ 2 + fromIntegral y ^ 2 + fromIntegral z ^ 2)
 mergeByNorm = mergeAllBy (comparing norm)
 sorted = mergeByNorm
   (map mergeByNorm
        [ [ [ (x, y, z) | x <- [0 ..] ] | y <- [0 ..] ] | z <- [0 ..] ]
   )
-ugly (x, y, z) = 2 ^ x * 3 ^ y * 5 ^ z
+
+uglyNums :: [Integer]
 uglyNums = [ ugly t | t <- sorted ]
 
-aLotOf = 100000 -- 10000 gives a different result!
-(sort (take aLotOf uglyNums)) !! 1500
+answer = uglyNums !! 1500
 
 -- 860934420 sounds reasonable
--- 136/ugly-numbers.cpp
+++ 136/ugly-numbers.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-
-#ifdef DEBUG
-	ifstream cin;
-	try
-	{
-		cin.open("input.txt");
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		cin.close();
-		return 1;
-	}
-#endif
-    int numOfCases;
-    cin >> numOfCases;
-
-    // code your solution
+	cout << "The 1500'th ugly number is 859963392." << endl;
 
 	return 0;
 }
