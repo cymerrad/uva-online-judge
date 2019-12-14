b2530f163288234c2382b6c6a7cca48b12309d7b
cymerrad <cymerrad@gmail.com>
2019-12-13 19:09:23 -0800

    Remove unnecessary comments

--- 136/lol.hs
+++ 136/lol.hs
@@ -8,27 +8,16 @@ import           Prelude
 import           System.IO
 import           Data.List.Ordered
 import           Data.Ord
--- data-ordlist
 
 main :: IO ()
-
 main = do
-  -- handle   <- openFile "nums.txt" WriteMod
-  -- contents <- hGetContents handle
-  -- hClose handle
-  -- writeFile "num.txt"
-  --   $ foldl (\a b -> a ++ b ++ "\n") "" (map show (take 10000 uglyNums))
-  print (uglyNums !! 1498)
-  print (uglyNums !! 1499)
-  print (uglyNums !! 1500)
-  print (uglyNums !! 1501)
+  print (answer)
   return ()
 
+-- UGLY IS THE NORM!
+norm :: (Integer, Integer, Integer) -> Integer
+norm (x, y, z) = 2 ^ x * 3 ^ y * 5 ^ z
 
-ugly :: (Integer, Integer, Integer) -> Integer
-ugly (x, y, z) = 2 ^ x * 3 ^ y * 5 ^ z
-norm = ugly -- UGLY IS THE NORM!
--- norm (x,y,z) = sqrt (fromIntegral x ^ 2 + fromIntegral y ^ 2 + fromIntegral z ^ 2)
 mergeByNorm = mergeAllBy (comparing norm)
 sorted = mergeByNorm
   (map mergeByNorm
@@ -40,4 +29,4 @@ uglyNums = [ ugly t | t <- sorted ]
 
 answer = uglyNums !! 1500
 
--- 860934420 sounds reasonable
+-- it should be 860934420
